21e3b15494d3a2e1f1bd5a956947dd25e5ce1011
SeanMichael Galvin <s.y.galvin@spartans.nsu.edu>
2013-03-22 11:01:48 -0400

    Corrected excessive markers

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
-var map, count, markers=[], listings=[];
-var infoWindow = new google.maps.InfoWindow;
+
 
 $(document).ready(function () {
+	var map, count, markers=[], listings=[];
+var infoWindow = new google.maps.InfoWindow;
 
 	function initializeMap() {
 		var mapOptions = {
@@ -14,8 +15,8 @@ $(document).ready(function () {
 		google.maps.event.trigger(map, 'resize');
 	}
 
-
 	function getLatLong(locationString)	{
+				
 				var geocoder = new google.maps.Geocoder();
 				geocoder.geocode({address:locationString}, function(results, status){
 				locate = results[0];
@@ -34,17 +35,21 @@ $(document).ready(function () {
 					console.log('error');
 				   },
 				   success: function(response) {
-					var results = response.results, listings = [], markers = [];
+					for (var i = 0; i < markers.length; i++ ) {
+						markers[i].setMap(null);
+					}
+					var results = response.results, listings = [];
 					for(var idx in results){
 						var result = results[idx];
 						var marker = new google.maps.Marker({'position': new google.maps.LatLng(result.latitude, result.longitude), 'map':map});
+						listings.push(result);
+						markers.push(marker);
 						google.maps.event.addListener(marker, 'click', function () {
 							var idx = markers.indexOf(this), listing = listings[idx];			
 							infoWindow.setOptions({"position": this.position, "content": "<img src=\"" + listing.images.url[0] + '" style="width:50px;height:50px;float:left;" /></br>' + listing.name + '</br>' + listing.phone}); 
 							infoWindow.open(map, this);
 						});
-						listings.push(result);
-						markers.push(marker);
+						
 					}
 					console.log(markers.length);
 				   }
